474e2c061e740566d53cb850101d7284cb8f5c0e
swadesh sedai <swadeshsedai@swadeshs-MacBook-Pro.local>
2018-04-22 01:59:59 +1000

    Upload new

---
+++ services/serverMigration/mig2.js
@@ -0,0 +1,13 @@
+
+$('#button1').on("click",function()
+     	{
+     		$("#hiding").fadeToggle("slow","linear");
+     	});
+$('#button2').on("click",function()
+     	{
+     		$("#hiding1").fadeToggle("slow","linear");
+     	});
+$('#button3').on("click",function()
+     	{
+     		$("#hiding2").fadeToggle("slow","linear");
+     	});
